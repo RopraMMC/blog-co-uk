56e8c3acefa55704c5d6d398f645d4262bc6da16
Ropra <rajinderropra@gmail.com>
2022-08-26 01:27:26 +0100

    starting PHP and SQL for login systems.

---
+++ admin_page.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./index.css">
+    <title>Ropra • Blog</title>
+    <link rel="icon" type="image/x-icon" href="Studio-Project.png">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- ====== Fontawesome CDN Link ====== -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"/>
+</head>
+<body>
+
+</body>
