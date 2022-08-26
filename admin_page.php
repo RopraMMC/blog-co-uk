4e75948cb3f8727bf28516db21dcf5e77b38f1b0
Ropra <rajinderropra@gmail.com>
2022-08-26 01:47:04 +0100

    update

--- admin_page.php
+++ admin_page.php
@@ -12,5 +12,28 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"/>
 </head>
 <body>
+    <div class="container">
+        <div class="home-page">
+            <header>
+                <div class="logo">
+                    <a href="index.html">Ropra • BLOG</a>
+                </div>
+                <div class="menu">
+                    <ul>
+                        <li><a href="index.html">home</a></li>
+                        <li><a href="blog.html">blog</a></li>
+                        <li><a href="index.html#contact">contact</a></li>
+                        <li><a href="#">sign up</a></li>
+                        <li><a href="#">sign in</a></li>
+                    </ul>
+                </div>
+            </header>
+            <div class="contact-content">
+                <div class="side-right">
+                    <h1 class="topic-text">Hello Admin!</h1>
 
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
