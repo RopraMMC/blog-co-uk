f4a5aa34ad607c345517783286cad5b3f3634b18
Ropra <rajinderropra@gmail.com>
2022-09-01 23:01:32 +0100

    register_form.php && css

--- register_form.php
+++ register_form.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="./index.css">
+    <link rel="stylesheet" href="./register_form.css">
     <title>Ropra • Blog</title>
     <link rel="icon" type="image/x-icon" href="Studio-Project.png">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -13,3 +14,27 @@
 </head>
 <body>
 <?php include "header.php"; ?>
+    <div class="form-container">
+        <h1 class="topic-text">Register Now</h1>
+        <p>Sign up for completely free today!</p>
+        <form action="" method="POST">
+            <div class="input-box">
+                <input type="text" name="Name" placeholder="Enter Your First And Last Name">
+            </div>
+            <div class="input-box">
+                <input type="text" name="Email" placeholder="Enter Your Email">
+            </div>
+            <div class="input-box">
+                input text="password" name="Password" placeholder="Enter Your Password">
+            </div>
+            <div class="input-box">
+                input text="password" name="cPassword" placeholder="Confirm Your Password">
+            </div>
+            <div class="button">
+                <input type="submit" value="Register Now">
+            </div>
+            <div class="login-text">
+                <p>Already have an account? <a href="login.php">Login here</a></p>
+            </div>
+        </div>
+    </div>
